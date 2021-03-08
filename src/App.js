b3b678179db63cd21f66da30efa9c2945a9894ba
DavideNas <d.naselli@gmail.com>
2021-03-08 17:11:33 +0100

    add state sample with increment of number

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Messages from './Messages';
 import Post from './FacebookPost';
 
-function App() {
-  console.log('hello from App.js')
-  return (
-    <div>
-      <h2>Ciao Androide</h2>
-      <button onClick={() => console.log('hello')}>Click me</button>
-      <Messages />
-      <Post author="Mr. Henky" description="è una cacca ?" />
-      <Post author="Dr. Henky" description="è una merdina..." />
-      <Post author="Mrs. Henky" description="è una vera merda !"/>
-      <Post author="Henky Kid" description="io la faccio profumata"/>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    number: 0,
+  }
+
+  increment = () => {
+    this.setState({
+      number: this.state.number +1
+    })
+  }
+  render() {
+    return (
+      <div className="Container">
+        <h3>counter: {this.state.number}</h3>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default App;
